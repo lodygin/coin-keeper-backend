3443bd93cbc5a3068e2b0e1bc148f55c316648d0
lodygin <lodigin.alexander@gmail.com>
2023-05-30 23:57:32 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const PORT = 5050;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json('Server is working!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on the port ${PORT}`);
+});
