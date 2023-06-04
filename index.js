4398c7e1741ce3848e9d4377fe955cee2d7a80ca
lodygin <lodigin.alexander@gmail.com>
2023-06-04 20:32:13 +0300

    feature: connect mongodb
    Close #3

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json('Server is working!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on the port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(PORT, () => console.log(`Server started on the port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
