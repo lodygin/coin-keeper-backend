bef3278cd0c0891e4a3212d033866f14f08fcad1
lodygin <lodigin.alexander@gmail.com>
2023-06-04 22:48:13 +0300

    feature: create basic logic of registration
    Close #5

---
+++ services/user.service.js
@@ -0,0 +1,45 @@
+const UserModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail.service');
+const tokenService = require('./token.service');
+const UserDto = require('../dtos/user.dto');
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+
+    if (candidate) {
+      throw new Error('User already exists');
+    }
+
+    const hashPassword = await bcrypt.hash(password.toString(), 3);
+    const activationLink = uuid.v4();
+    const user = await UserModel.create({ email, hashPassword, activationLink });
+
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/activate/${activationLink}`,
+    );
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne({ activationLink });
+
+    if (!user) {
+      throw new Error('Incorrect link');
+    }
+
+    user.isActivated = true;
+    await user.save();
+  }
+}
+
+module.exports = new UserService();
